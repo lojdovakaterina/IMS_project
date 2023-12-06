19af1e856b984b6167a63cd6a1260f6a9529d4f0
AndyMichl <andrea.michlikova@seznam.cz>
2023-12-06 19:25:59 +0100

    base model
    litttle progress

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 // Bla TODO
 //
 
-#include <simlib.h>
 #include <cstdlib>
 #include <cstring>
+#include <simlib.h>
 
 int national = 0;
 int international = 0;
@@ -15,38 +15,72 @@ bool side_window = false;
 bool line_divide = false;
 bool self_checkout = false;
 
+#define WAIT_QUEUE 3
+
+Store pokladny("Pokladny", 0);
+
 // global objects:
-Facility Box("Box");
+// Facility Box("Box");
 Histogram Table("Table", 0, 25, 20);
+
+
+class NationalConnections : public Process
+{
+    void Behavior()
+    {
+        Enter(pokladny, 1);
+        Wait(Exponential(1)); // u pokladny // 37.13 s TODO
+        Leave(pokladny, 1);
+    }
+};
+
+class InternationalConnections : public Process
+{
+    void Behavior()
+    { 
+        Enter(pokladny, 1);
+        Wait(Exponential(2)); // u pokladny // 43.58 s TODO
+        Leave(pokladny, 1);
+    }
+};
 
 class Customer : public Process
 {
-    double Prichod; // atribute of each customer
+
+    // double Prichod; // atribute of each customer
     void Behavior()
-    {                          // --- behavoir specification ---
-        Prichod = Time;        // incoming time
-        Seize(Box);            // start of service
-        Wait(10);              // time of service
-        Release(Box);          // end of service
-        Table(Time - Prichod); // waiting and service time
+    {                     // --- behavoir specification ---
+        // Prichod = Time;   // incoming time
+        // Seize(Box);       // start of service
+        Wait(WAIT_QUEUE); // time of service       //TODO vteriny?
+        double p = Uniform(0, 100);
+        if (p > 45.39)
+        {
+            (new NationalConnections)->Activate();
+        }
+        else
+        {
+            (new InternationalConnections)->Activate();
+        }
+        // Release(Box);          // end of service
+        // Table(Time - Prichod); // waiting and service time
     }
 };
 
 class Generator : public Event
-{ // model of system's input
+{ // generátor zákazníků
     void Behavior()
-    {                                            // --- behavior specification ---
-        (new Customer)->Activate();              // new customer
-        Activate(Time + Exponential(1e3 / 150)); //
+    {                                    // --- popis chování  generátorů ---
+        (new Customer)->Activate();      // nový zákazník, aktivace v čase time
+        Activate(Time + Exponential(5)); // příchod cestujících do systému TODO
     }
 };
-
 
 void argParse(int argc, char *argv[])
 {
     if ((argc == 2 && (strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-h") == 0)) || argc < 5)
     {
-        printf("Použití: [-i] international [-n] national [-b] [-sd] [-ld] [-sc]\n"      //TODOO
+        printf("Použití: [-i] international [-n] national [-b] [-sd] [-ld] [-sc]\n" // TODOO
                "[-i] Počet otevřených mezinárodních přepážek\n"
                "[-n] Počet otevřených vnitrostátních přepážek\n"
                "[-b] Základní model\n"
@@ -98,7 +132,8 @@ void argParse(int argc, char *argv[])
             std::exit(EXIT_SUCCESS);
         }
     }
-    if (international == 0 || national == 0){
+    if (international == 0 || national == 0)
+    {
         printf("Nutné zadat parametr -i a -n");
         std::exit(EXIT_SUCCESS);
     }
@@ -108,16 +143,17 @@ int main(int argc, char *argv[])
 {
     argParse(argc, argv);
 
-    if (base){
-        printf("Začátek simulace...\n");
+    pokladny.SetCapacity(national);
+    {
+        printf("Začátek simulace... %ld\n", pokladny.Capacity());
         SetOutput("base.out");
-        Init(0, 1000);               // experiment initialization for time 0..1000
+        Init(0, 100);               // experiment initialization for time 0..100
         (new Generator)->Activate(); // customer generator
         Run();                       // simulation
-        Box.Output();                // print of results
+        // Box.Output();                // print of results
         Table.Output();
         printf("Konec simulace...\n");
-    } 
+    }
     return 0;
 }
 /*** End of file main.cpp ***/
