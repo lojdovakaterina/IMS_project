2e255337dc219e6ee21f8d3bb2fb8c1d1593d6c4
AndyMichl <andrea.michlikova@seznam.cz>
2023-12-10 14:35:10 +0100

    skripty

---
+++ skript.py
@@ -0,0 +1,114 @@
+import os
+import re
+
+# Vytvořte funkci pro výpočet průměrů z jednoho souboru
+def calculate_avgs(file_path, filename):
+    with open(file_path, 'r') as file:
+        data = file.read()
+
+    # Rozdělte vstup na záznamy
+    records = re.split(r'(?<=\bBASE MODEL\b)', data)
+
+    # Inicializujte proměnné pro průměr
+    total_iw = 0
+    total_nw = 0
+
+    total_ic = {}
+    total_nc = {}
+
+    # Pro každý záznam vypočtěte průměr
+    for record in records:
+        count_iw = 0
+        count_nw = 0
+        count_ic = {}   
+        count_nc = {}
+
+        lines = record.split('\n')
+        # Check for insufficient lines
+        if len(lines) < 3:
+            print(f"Skipping record due to insufficient lines: {lines}")
+            continue
+
+        for line in lines :
+            if line.startswith('IW'):
+                iw_value = float(line.split()[1])
+                total_iw += iw_value
+                count_iw += 1
+
+            elif line.startswith('NW'):
+                nw_value = float(line.split()[1])
+                total_nw += nw_value
+                count_nw += 1
+
+            elif line.startswith('IC'):
+                ic_number = line.split()[0][2:]  # Extract the number after 'ic'
+                ic_value = float(line.split()[1])
+
+                # Initialize counter for this ic number if not present
+                if ic_number not in count_ic:
+                    count_ic[ic_number] = 0
+
+                total_ic[ic_number] = total_ic.get(ic_number, 0) + ic_value
+                count_ic[ic_number] += 1
+
+            elif line.startswith('NC'):
+                nc_number = line.split()[0][2:]  # Extract the number after 'nc'
+                nc_value = float(line.split()[1])
+
+                # Initialize counter for this nc number if not present
+                if nc_number not in count_nc:
+                    count_nc[nc_number] = 0
+
+                total_nc[nc_number] = total_nc.get(nc_number, 0) + nc_value
+                count_nc[nc_number] += 1
+
+        avg_iw = total_iw / count_iw if count_iw > 0 else None
+        avg_nw = total_nw / count_nw if count_nw > 0 else None
+
+        result_string = f"----------------- File: {filename} -----------------\n"
+
+        total_avg_ic = 0        
+        total_avg_nc = 0       
+        total_count_ic = 0        
+        total_count_nc = 0       
+        for i, count in count_ic.items():
+            avg_ic = total_ic[i] / count
+            result_string += f"IC{i}: {avg_ic*100:.2f} %\n"
+            total_avg_ic += total_ic[i]
+            total_count_ic += count
+
+        result_string += f"-----------------\n"
+        for n, count in count_nc.items():
+            avg_nc = total_nc[n] / count
+            result_string += f"NC{n}: {avg_nc*100:.2f} %\n"
+            total_avg_nc += total_nc[n]
+            total_count_nc += count
+
+        result_string += f"-----------------\n"
+        total_avg_ic = total_avg_ic / total_count_ic
+        total_avg_nc = total_avg_nc / total_count_nc
+
+        result_string += f"IC: {total_avg_ic*100:.2f} %\n"
+        result_string += f"NC: {total_avg_nc*100:.2f} %\n"
+        result_string += f"IW: {avg_iw:.2f} s\n"
+        result_string += f"NW: {avg_nw:.2f} s\n"
+        return result_string
+
+
+# Nastavte cestu k adresáři se soubory
+script_directory = os.path.dirname(os.path.abspath(__file__))
+directory_path = out_directory = os.path.join(script_directory, 'out')
+
+# Projděte všechny soubory v adresáři
+for filename in os.listdir(directory_path):
+    if filename.startswith("base") and filename.endswith(".out"):
+        file_path = os.path.join(directory_path, filename)
+
+        # Vypočtěte průměry z jednoho souboru
+        result = calculate_avgs(file_path, filename)
+
+        # Otevřete soubor pro přidání na konec
+        output_file = file_path = os.path.join(directory_path, "prumerW.out")
+        with open(file_path, 'a') as file:
+            # výsledky na konec souboru
+            file.write(result)
---
+++ skript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Projdi všechny kombinace -i a -n
+for i in {1..10}; do
+    for i in {1..4}; do
+        for n in {1..6}; do
+            # Vytvoř název výstupního souboru
+            output_filename="base_$i$n.out"
+
+            # Spusť příkaz s odpovídajícími přepínači
+            ./test -i $i -n $n -b >> out/$output_filename
+        done
+    done
+    sleep 1
+done
